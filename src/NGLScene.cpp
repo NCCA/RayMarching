40f3bdd58ec938f14b9a2d7865b121677dc0a30b
jmacey <jmacey@bournemouth.ac.uk>
2022-11-11 12:38:51 +0000

    updated to include iostream as it was inadvertently added in ngl

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -7,25 +7,22 @@
 #include <ngl/VAOPrimitives.h>
 #include <ngl/ShaderLib.h>
 #include <QTime>
-
-
-
+#include <iostream>
 
 NGLScene::NGLScene()
 {
   setTitle("Ray Marching");
 }
 
-
 NGLScene::~NGLScene()
 {
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-void NGLScene::resizeGL( int _w, int _h )
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
-  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 
 void NGLScene::initializeGL()
@@ -34,7 +31,7 @@ void NGLScene::initializeGL()
   // gl commands from the lib, if this is not done program will crash
   ngl::NGLInit::initialize();
 
-  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
@@ -42,37 +39,33 @@ void NGLScene::initializeGL()
   // we are creating a shader called RayMarch
   ngl::ShaderLib::createShaderProgram("RayMarch");
   // now we are going to create empty shaders for Frag and Vert
-  ngl::ShaderLib::attachShader("RayMarchVertex",ngl::ShaderType::VERTEX);
-  ngl::ShaderLib::attachShader("RayMarchFragment",ngl::ShaderType::FRAGMENT);
+  ngl::ShaderLib::attachShader("RayMarchVertex", ngl::ShaderType::VERTEX);
+  ngl::ShaderLib::attachShader("RayMarchFragment", ngl::ShaderType::FRAGMENT);
   // attach the source
-  ngl::ShaderLib::loadShaderSource("RayMarchVertex","shaders/RayMarchVertex.glsl");
-  ngl::ShaderLib::loadShaderSource("RayMarchFragment","shaders/RayMarchFragment.glsl");
+  ngl::ShaderLib::loadShaderSource("RayMarchVertex", "shaders/RayMarchVertex.glsl");
+  ngl::ShaderLib::loadShaderSource("RayMarchFragment", "shaders/RayMarchFragment.glsl");
   // compile the shaders
   ngl::ShaderLib::compileShader("RayMarchVertex");
   ngl::ShaderLib::compileShader("RayMarchFragment");
   // add them to the program
-  ngl::ShaderLib::attachShaderToProgram("RayMarch","RayMarchVertex");
-  ngl::ShaderLib::attachShaderToProgram("RayMarch","RayMarchFragment");
+  ngl::ShaderLib::attachShaderToProgram("RayMarch", "RayMarchVertex");
+  ngl::ShaderLib::attachShaderToProgram("RayMarch", "RayMarchFragment");
   ngl::ShaderLib::linkProgramObject("RayMarch");
   ngl::ShaderLib::use("RayMarch");
-  ngl::ShaderLib::setUniform("mouse",ngl::Vec2(23,23));
-  ngl::ShaderLib::setUniform("cam_pos",ngl::Vec3(0.0,0.0,4.0));
-  ngl::ShaderLib::setUniform("time",0.0f);
+  ngl::ShaderLib::setUniform("mouse", ngl::Vec2(23, 23));
+  ngl::ShaderLib::setUniform("cam_pos", ngl::Vec3(0.0, 0.0, 4.0));
+  ngl::ShaderLib::setUniform("time", 0.0f);
 
-
-  m_screenQuad.reset( new ScreenQuad("RayMarch"));
+  m_screenQuad.reset(new ScreenQuad("RayMarch"));
   // as re-size is not explicitly called we need to do this.
-  glViewport(0,0,width(),height());
+  glViewport(0, 0, width(), height());
   startTimer(10);
-
 }
-
 
 void NGLScene::loadMatricesToShader()
 {
-  ngl::ShaderLib::setUniform("mouse",ngl::Vec2(m_win.spinXFace,m_win.spinYFace));
-  ngl::ShaderLib::setUniform("cam_pos",ngl::Vec3(0.0,0.0,4.0));
-
+  ngl::ShaderLib::setUniform("mouse", ngl::Vec2(m_win.spinXFace, m_win.spinYFace));
+  ngl::ShaderLib::setUniform("cam_pos", ngl::Vec3(0.0, 0.0, 4.0));
 }
 
 void NGLScene::paintGL()
@@ -94,29 +87,40 @@ void NGLScene::keyPressEvent(QKeyEvent *_event)
   switch (_event->key())
   {
   // escape key to quite
-  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Escape:
+    QGuiApplication::exit(EXIT_SUCCESS);
+    break;
   // turn on wirframe rendering
-  case Qt::Key_W : glPolygonMode(GL_FRONT_AND_BACK,GL_LINE); break;
+  case Qt::Key_W:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    break;
   // turn off wire frame
-  case Qt::Key_S : glPolygonMode(GL_FRONT_AND_BACK,GL_FILL); break;
+  case Qt::Key_S:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    break;
   // show full screen
-  case Qt::Key_F : showFullScreen(); break;
+  case Qt::Key_F:
+    showFullScreen();
+    break;
   // show windowed
-  case Qt::Key_N : showNormal(); break;
-  default : break;
+  case Qt::Key_N:
+    showNormal();
+    break;
+  default:
+    break;
   }
   // finally update the GLWindow and re-draw
-  //if (isExposed())
-    update();
+  // if (isExposed())
+  update();
 }
 
 void NGLScene::timerEvent(QTimerEvent *_event)
 {
-  static float t=0.0;
+  static float t = 0.0;
   ngl::ShaderLib::use("RayMarch");
-  ngl::ShaderLib::setUniform("time",t);
-  t+=0.01;
-  if(t > 5.0)
-    t=0.0;
+  ngl::ShaderLib::setUniform("time", t);
+  t += 0.01;
+  if (t > 5.0)
+    t = 0.0;
   update();
 }
